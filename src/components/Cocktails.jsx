4dcbcc8496ebe17b067999e7bf3f85890095191f
Esraa Abduallah <esraahisham753@gmail.com>
2025-09-09 20:02:03 +0300

    Enhance Cocktails component with layout and images; add section for popular cocktails

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { cocktailLists } from '../../constants/index';
 
 const Cocktails = () => {
   return (
-    <div>Cocktails</div>
+    <section id='cocktails' className='noisy'>
+      <img 
+        src='/images/cocktail-left-leaf.png'
+        alt='l-leaf'
+        className='c-left-leaf'
+      />
+      <img 
+        src='/images/cocktail-right-leaf.png'
+        alt='r-leaf'
+        className='c-right-leaf'
+      />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most popular cocktails:</h2>
+          <ul>
+            
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 

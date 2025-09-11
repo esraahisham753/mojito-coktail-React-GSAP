10398fcced0d99adff5049e6f326e7ddea33e702
Esraa Abduallah <esraahisham753@gmail.com>
2025-09-11 18:57:29 +0300

    Add Art component to App layout; implement GSAP animations in About component for enhanced visual effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,81 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import { SplitText } from "gsap/all"
 
 const About = () => {
+  useGSAP(() => {
+    const titleWords = new SplitText('#about h2', {
+        type: 'words'
+    });
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#about',
+            start: 'top center'
+        }
+    });
+
+   tl.from(titleWords.words, {
+    opacity: 0,
+    duration: 1,
+    ease: 'expo.inOut',
+    stagger: 0.02,
+    yPercent: 100
+   });
+
+   tl.from('.top-grid div, .bottom-grid div', {
+    opacity: 0,
+    duration: 1,
+    ease: 'power1.inOut',
+    stagger: 0.04
+   }, '-=0.5')
+    
+  })
  
   return (
     <section id="about">
-        <div className="content">
-            <div className="md:col-span-8">
-                <div className="mb-16 md:px-0 px-2">
-                    <p className="badge">Best Cocktails</p>
-                    <h2>Where every detail matters<span className="text-white-100">-</span>from muddle to garnish</h2>
+        <div className="mb-16 md:px-5 px-0">
+            <div className="content">
+                <div className="md:col-span-8">
+                    <div className="mb-16 md:px-0 px-2">
+                        <p className="badge">Best Cocktails</p>
+                        <h2>Where every detail matters<span className="text-white">-</span>from muddle to garnish</h2>
+                    </div>
+                </div>
+                <div className="sub-content">
+                    <p>Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
+                    <div>
+                        <p className="md:text-3xl text-xl font-bold">
+                            <span>4.5</span>/5
+                        </p>
+                        <p className="text-sm text-white-100">More than +12000 customers</p>
+                    </div>
+                
                 </div>
             </div>
-            <div className="sub-content">
-                <p>Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable. </p>
-                <div>
-                    <span className="md:text-3xl text-xl">4.5/5</span>
-                    <p className="text-sm text-white-100">More than +12000 customers</p>
-                </div>
+        </div>
+        <div className="top-grid">
+            <div className="md:col-span-3">
+                <div className="noisy"/>
+                <img src="/images/abt1.png" alt="cocktail image"/>
+            </div>
+            <div className="md:col-span-3">
+                <div className="noisy"/>
+                <img src="/images/abt2.png" alt="cocktail image"/>
+            </div>
+            <div className="md:col-span-6">
+                <div className="noisy"/>
+                <img src="/images/abt5.png" alt="cocktail image"/>
+            </div>
+        </div>
+        <div className="bottom-grid">
+            <div className="md:col-span-9">
+                <div className="noisy"/>
+                <img src="/images/abt3.png" alt="cocktail image"/>
+            </div>
+            <div className="md:col-span-3">
+                <div className="noisy"/>
+                <img src="/images/abt4.png" alt="cocktail image"/>
             </div>
         </div>
     </section>
